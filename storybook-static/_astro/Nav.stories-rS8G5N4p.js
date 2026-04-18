a4758bae2ff4af781a4e3c45f035b2c2c31cf805
Jamal Williams <jamal@jamalwilliams.com>
2026-04-18 14:46:11 -0400

    chore: cleanup repo for initial push (v1.3.1)

--- storybook-static/_astro/Nav.stories-rS8G5N4p.js
+++
@@ -1,3 +0,0 @@
-const o=()=>{throw new Error("Astro components are rendered server-side by Storybook.")};o.isAstroComponentFactory=!0;o.moduleId="/Users/jamalwilliams/code/anarrativelens.com/src/components/Nav.astro";const r={title:"Components/Nav",component:o},e={args:{}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const s=["Default"];export{e as Default,s as __namedExportsOrder,r as default};
--- storybook-static/_astro/PortfolioSection.stories-S3TpV2zm.js
+++
@@ -1,6 +0,0 @@
-const o=()=>{throw new Error("Astro components are rendered server-side by Storybook.")};o.isAstroComponentFactory=!0;o.moduleId="/Users/jamalwilliams/code/anarrativelens.com/src/components/PortfolioSection.astro";const t={title:"Components/PortfolioSection",component:o,argTypes:{heading:{control:"text"},subheading:{control:"text"},projects:{control:"object"}}},e={args:{heading:"Featured Stories",subheading:"A glimpse into the narratives we've helped build and capture."}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    heading: 'Featured Stories',
-    subheading: 'A glimpse into the narratives we\\'ve helped build and capture.'
-  }
-}`,...e.parameters?.docs?.source}}};const r=["Default"];export{e as Default,r as __namedExportsOrder,t as default};
